81c33d29113288e0cdd5765af97e810ad5306e35
Talukanyani <tmutshaeni@hotmail.com>
2024-01-20 19:18:21 +0200

    home page header

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import Navbar from '../../components/Navbar'
+import Header from './components/Header'
 
 export default function HomePage() {
     useEffect(() => {
@@ -9,7 +10,7 @@ export default function HomePage() {
     return (
         <>
             <Navbar />
-            <h1>Home Page</h1>
+            <Header />
         </>
     )
 }
---
+++ src/pages/home/components/Header.js
@@ -0,0 +1,31 @@
+import styles from './Header.module.css'
+import play_store_img from '../../../assets/images/play_store.svg'
+import app_store_img from '../../../assets/images/app_store.svg'
+
+export default function Header() {
+    return (
+        <header id='header' className={styles.header}>
+            <div>
+                <h1>Student Calendar</h1>
+                <h2>
+                    Stay Organised and on Track.
+                    Manage your Academic Assessments.
+                </h2>
+                <section className={styles.download_btns}>
+                    <button>
+                        <img
+                            src={play_store_img}
+                            alt='Play Store buttton'
+                        />
+                    </button>
+                    <button>
+                        <img
+                            src={app_store_img}
+                            alt='App Store buttton'
+                        />
+                    </button>
+                </section>
+            </div>
+        </header>
+    )
+}
