afa03f0e590036e568bb59781e11d96de7d455fa
Talukanyani <tmutshaeni@hotmail.com>
2024-01-23 23:38:32 +0200

    404 page

--- src/pages/error/PageNotFound.js
+++ src/pages/error/PageNotFound.js
@@ -1,4 +1,7 @@
+import styles from './PageNotFound.module.css'
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from '../../components/Navbar'
 
 export default function PageNotFound() {
     useEffect(() => {
@@ -6,6 +9,16 @@ export default function PageNotFound() {
     })
 
     return (
-        <h1>404 - Page Not Found</h1>
+        <>
+            <Navbar />
+            <div className={styles.page_not_found}>
+                <h1>404</h1>
+                <h2>Ooops! Page Not Found</h2>
+                <p>We can not find the page you are looking for.</p>
+                <Link to='/' onClick={() => window.scrollTo(0, 0)}>
+                    <button className='primary_btn'>Home Page</button>
+                </Link>
+            </div>
+        </>
     )
 }
